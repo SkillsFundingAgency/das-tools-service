4309ea7997c73b5732671e20d3da299bbd9743ce
Craig Gumbley <craiggumbley@gmail.com>
2020-01-24 20:35:47 +0000

    prepare controler for conditional rendering

---
+++ src/SFA.DAS.ToolService.Web/Models/ToolsHomeViewModel.cs
@@ -0,0 +1,8 @@
+namespace SFA.DAS.ToolService.Web.Models
+{
+    public class ToolsHomeViewModel
+    {
+        public string UserName { get; set; }
+        public bool HasApprenticeshipsRole { get; set; }
+    }
+}
