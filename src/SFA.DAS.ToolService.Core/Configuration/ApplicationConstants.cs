c99a5a9457da4a178cd0933382282e81c6adc97d
Craig Gumbley <craiggumbley@gmail.com>
2020-02-26 14:36:06 +0000

    Fix applicationname const

--- src/SFA.DAS.ToolService.Core/Configuration/ApplicationConstants.cs
+++ src/SFA.DAS.ToolService.Core/Configuration/ApplicationConstants.cs
@@ -2,7 +2,7 @@ namespace SFA.DAS.ToolService.Core.Configuration
 {
     public static class ApplicationConstants
     {
-        public const string ApplicationName = "das-tool-service";
+        public const string ApplicationName = "das-tools-service";
         public const string AdminRole = "Admin";
     }
 }
