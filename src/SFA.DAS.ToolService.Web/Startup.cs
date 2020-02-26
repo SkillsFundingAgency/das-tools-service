5d47d9a77b76b237278b3312422202e3a3fb2e6e
Craig Gumbley <craiggumbley@gmail.com>
2020-02-26 13:25:12 +0000

    Ensure XForwardedHeaders are set

--- src/SFA.DAS.ToolService.Web/Startup.cs
+++ src/SFA.DAS.ToolService.Web/Startup.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -98,6 +98,11 @@ namespace SFA.DAS.ToolService.Web
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+                    {
+                        ForwardedHeaders = ForwardedHeaders.XForwardedProto
+                    });
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
