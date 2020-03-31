f84ebe4fad6d2dd8e9eb9f4f19ae0477e1746836
Craig Gumbley <craiggumbley@gmail.com>
2020-03-31 09:06:56 +0100

    Re work home page and add dfesigninaddress (#32)
    * Re work home page and add dfesigninaddress
    
    * Fix Dsi link
    
    * Address PR comments and use IOptions for config

--- src/SFA.DAS.ToolService.Web/Controllers/HomeController.cs
+++ src/SFA.DAS.ToolService.Web/Controllers/HomeController.cs
@@ -1,18 +1,31 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using SFA.DAS.ToolService.Core.Configuration;
+using SFA.DAS.ToolService.Web.Models;
 
 namespace SFA.DAS.ToolService.Web.Controllers
 {
     [AllowAnonymous]
     public class HomeController : BaseController<HomeController>
     {
-        public HomeController()
+        private readonly IOptions<HomePageLinksConfiguration> links;
+
+        public HomeController(IOptions<HomePageLinksConfiguration> _links)
         {
+            links = _links;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var model = new HomeViewModel
+            {
+                DfeSignInAddress = links.Value.DfESignInAddress,
+                GitHubOrgAddress = links.Value.GitHubOrgAddress
+            };
+
+            return View(model);
         }
     }
 }
