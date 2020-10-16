a5f210cd3cf4db6c72d5fd12aa5cbd83ef9e127c
Mohamad Adam <accadam230594@outlook.com>
2020-10-16 11:38:31 +0100

    Remove signin config and replace with AAD

--- src/SFA.DAS.ToolService.Core/Configuration/HomePageLinksConfiguration.cs
+++ src/SFA.DAS.ToolService.Core/Configuration/HomePageLinksConfiguration.cs
@@ -2,8 +2,6 @@ namespace SFA.DAS.ToolService.Core.Configuration
 {
     public class HomePageLinksConfiguration
     {
-        public string DfESignInAddress { get; set; }
-
         public string GitHubOrgAddress { get; } = ApplicationConstants.GitHubOrgAddress;
     }
 }
